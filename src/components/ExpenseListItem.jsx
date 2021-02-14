35045fb6837440f45348a4a75594acad7a990829
chukwudinweze <chukwudinwez@gmail.com>
2021-02-14 11:27:09 -0800

    3 files modified, 1 deleted

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "./actions/expense";
 
-const ExpenseListItem = ({ description, amount, createdAt, note }) => (
+const ExpenseListItem = ({
+  dispatch,
+  id,
+  description,
+  amount,
+  createdAt,
+  note
+}) => (
   <div>
     <p>{description}</p>
     <p>{amount}</p>
     <p>{createdAt}</p>
     <p>{note}</p>
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove Expense
+    </button>
   </div>
 );
 
-export default ExpenseListItem
+export default connect()(ExpenseListItem);
